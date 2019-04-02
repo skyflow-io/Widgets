f4dd44d46412fdf54a139c1d238ae003a5440ac8
nsbx <nsbx@hotmail.fr>
2019-04-02 18:39:14 +0200

    fix(ci): restore previous code

--- ci/build.php
+++ ci/build.php
@@ -9,13 +9,34 @@ $fileSystem = new Filesystem();
 
 $result = [];
 
+$composeFinder = new Finder();
+$composeFinder->directories()->depth(0)->in('/app/src')->sortByName();
+
+foreach ($composeFinder as $compose) {
+    $name = $compose->getBasename();
+    $files = [
+        'name' => $name,
+        'files' => []
+    ];
+    $filesFinder = new Finder();
+    $filesFinder->files()->in('/app/src/' . $name);
+    foreach ($filesFinder as $file) {
+        $files['files'][] = [
+            'directory' => $file->getRelativePath(),
+            'filename' => $file->getFilename(),
+            'contents' => $file->getContents(),
+        ];
+    }
+    $result[] = $files;
+}
+
+$fileSystem->appendToFile('/app/widgets.json', json_encode($result));
+
+
 $filesFinder = new Finder();
 $filesFinder->files()->name('data.json')->in('/app/doc');
-
 foreach ($filesFinder as $file) {
-
     $result = json_decode($file->getContents(), true)['classes'];
-
 }
 
 $fileSystem->appendToFile('/app/doc/widgets.json', json_encode($result));
