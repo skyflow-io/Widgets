1b750828fc7274f8009e0d5409b2f290c2fa9e58
nsbx <nsbx@hotmail.fr>
2019-04-02 15:08:43 +0200

    fix(ci): update ci fix error

--- ci/build.php
+++ ci/build.php
@@ -9,33 +9,13 @@ $fileSystem = new Filesystem();
 
 $result = [];
 
-$composeFinder = new Finder();
+$filesFinder = new Finder();
+$filesFinder->files()->name('data.json')->in('/app/doc');
 
-$composeFinder->directories()->depth(0)->in('/app/src')->sortByName();
+foreach ($filesFinder as $file) {
 
-foreach ($composeFinder as $compose) {
+    $result = json_decode($file->getContents(), true)['classes'];
 
-    $name = $compose->getBasename();
-
-    $files = [
-        'name' => $name,
-        'files' => []
-    ];
-
-    $filesFinder = new Finder();
-    $filesFinder->files()->in('/app/src/' . $name);
-
-    foreach ($filesFinder as $file) {
-
-        $files['files'][] = [
-            'directory' => $file->getRelativePath(),
-            'filename' => $file->getFilename(),
-            'contents' => $file->getContents(),
-        ];
-
-    }
-
-    $result[] = $files;
 }
 
-$fileSystem->appendToFile('/app/widgets.json', json_encode($result));
+$fileSystem->appendToFile('/app/doc/widgets.json', json_encode($result));
